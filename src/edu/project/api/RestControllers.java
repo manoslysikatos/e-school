c9de32822da89f9ec2d909b4576f455d5ad0ee32
Eleftheria <it21754@hua.gr>
2020-03-31 17:36:48 +0300

    Update RestControllers.java

--- src/edu/project/api/RestControllers.java
+++ src/edu/project/api/RestControllers.java
@@ -11,23 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class RestControllers {
 	
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String showHomePage() {
-		System.out.println("ready to show: home page");
-
-		String json = "{\"Title\": \"This is the home-page\"}";
-		return json;
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String showLogInForAll() {
-		System.out.println("ready to show: log in page");
-
-		return "This is the login-page";
-	}
-	
-	
 	@RequestMapping(value = "/login/main-menu-for-all", method = RequestMethod.GET, produces = "application/json",  consumes = "application/json")
 	public @ResponseBody Users showMainMenuForAll(HttpServletRequest request, Model model, HttpSession session) {
 		System.out.println("DS: ready to show: main menu for all users page");
