f162468fdfe1f900b48ec29012c671edefe3c008
Eleftheria <it21754@hua.gr>
2020-04-16 23:51:47 +0300

    Update DAO.java

--- src/edu/project/database/DAO.java
+++ src/edu/project/database/DAO.java
@@ -1,10 +1,7 @@
 package edu.project.database;
 
+import java.util.List;
 import javax.servlet.http.HttpSession;
-
-import org.bson.Document;
-
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 
 import edu.project.entity.*;
@@ -14,9 +11,9 @@ public interface DAO {
 	public MongoDatabase connect_to_database(); //establish connection with the database 
 	public String find_user(MongoDatabase database,String username); //finds the user in the database based on the given username 
 
-	public String find_Assignments_for_a_student(MongoDatabase database, HttpSession session); //finds the assignments that a user/student has to answer in the database based on the given 
+	public List<String> find_Assignments_for_a_student(MongoDatabase database, HttpSession session); //finds the assignments that a user/student has to answer in the database based on the given 
 
-	public String find_Quizes_for_a_student(MongoDatabase database, HttpSession session); //finds the quizes that a user/student has to answer in the database based on the given 
+	public List<String> find_Quizes_for_a_student(MongoDatabase database, HttpSession session); //finds the quizes that a user/student has to answer in the database based on the given 
 
 	
 
